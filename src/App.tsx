49950b2d88db7c214d6ebd473b42aff4a7ec3420
Sevil Zaimova <sevil.zaimova@mail.schwarz>
2020-09-16 09:14:34 +0300

    Autoincrement versioning on prebuild version script

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import { version } from "../package.json";
 
 function App() {
+  const appVersion = `${version}-${process.env.NODE_ENV}`;
   return (
     <div className="App">
       <h1 className="greeting">Hello, World!</h1>
+      <h3>This application version number is: v{appVersion}</h3>
     </div>
   );
 }
